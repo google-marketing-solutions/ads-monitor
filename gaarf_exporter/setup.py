556660f64b81140e7163bf8df8f1ed9edb510f64
Andrei Markin <amarkin@google.com>
2024-05-02 18:59:12 +0400

    Update gaarf dependency and bump to version 0.7.0
    Change-Id: I632b7ec9675e160a6698a5eb4caf1d72882fda5e

--- gaarf_exporter/setup.py
+++ gaarf_exporter/setup.py
@@ -23,7 +23,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
   name='gaarf-exporter',
-  version='0.7.0.rc1',
+  version='0.7.0',
   long_description=README,
   long_description_content_type='text/markdown',
   author='Google Inc. (gTech gPS CSE team)',
@@ -38,7 +38,7 @@ setup(
   ],
   packages=find_packages(),
   install_requires=[
-    'prometheus-client', 'google-ads-api-report-fetcher>=1.12.0'
+    'prometheus-client', 'google-ads-api-report-fetcher==1.13.2'
   ],
   setup_requires=['pytest-runner'],
   tests_requires=['pytest'],
